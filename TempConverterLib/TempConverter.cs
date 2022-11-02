f47b7b2b764a1541b8adff507ec3557eb13e9303
Wilham Ezequiel <wilham.sousa@argoseguros.com.br>
2022-11-02 17:57:53 -0300

    New projects tempConverter

---
+++ TempConverterLib/TempConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace TempConverterLib
+{
+    public static class TempConverter
+    {
+        public static decimal CelciusToFahrenheit(decimal celciusTemp)
+            => (celciusTemp * 1.8M) + 32M;
+
+        public static decimal FahrenheitToCelcius(decimal fahrenheitTemp)
+            => (fahrenheitTemp - 32M) / 1.8M;
+    }
+}
---
+++ TempConverterTest/TempConverterTest.cs
@@ -0,0 +1,23 @@
+using TempConverterLib;
+
+namespace TempConverterTest
+{
+    public class TempConverterTest
+    {
+        [Fact]
+        public void CelciusToFahrenheitOK()
+                    => Assert.Equal(32, TempConverter.CelciusToFahrenheit(0));
+
+        [Fact]
+        public void FahrenheitToCelciusOK()
+            => Assert.Equal(0, TempConverter.FahrenheitToCelcius(32));
+
+        [Theory]
+        [InlineData(32, 0)]
+        [InlineData(86, 30)]
+        [InlineData(90.5, 32.5)]
+        [InlineData(212, 100)]
+        public void FahrenheitToCelcius(decimal fTemp, decimal cTemp)
+            => Assert.Equal(fTemp, TempConverter.CelciusToFahrenheit(cTemp));
+    }
+}
